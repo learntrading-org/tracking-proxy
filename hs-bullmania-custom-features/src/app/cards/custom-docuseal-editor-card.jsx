0e85c756f764c9bc5acfd8c95328b922ced157f8
John Versus <johnvs.nagendra@gmail.com>
2026-09-21 18:28:34 +0530

    Update custom-docuseal-editor-card.jsx

--- hs-bullmania-custom-features/src/app/cards/custom-docuseal-editor-card.jsx
+++ hs-bullmania-custom-features/src/app/cards/custom-docuseal-editor-card.jsx
@@ -29,7 +29,7 @@ const PRESETS = {
     "Private Live Streams",
     "BullMania AI",
     "Private Market Analysis Videos",
-    "3 Private Coaching Session(s)"
+    "4 Private Coaching Sessions"
   ], null, 2),
   no_sessions: JSON.stringify([
     "BullMania Trading Courses",
@@ -50,7 +50,7 @@ const PRESETS = {
     "Private Live Streams",
     "BullMania AI",
     "Private Market Analysis Videos",
-    "3 Private Coaching Session(s)",
+    "4 Private Coaching Sessions",
     "2 Portfolio Review(s) by Ivan"
   ], null, 2)
 };
